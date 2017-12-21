bc9c71bd4de0cc0b15809ec4b283d9fab1045564
Martin Gaiser <martin-gaiser@t-online.de>
2017-12-21 20:00:53 +0100

    added rotations in upIn Method

--- avlTree/avlTree.cpp
+++ avlTree/avlTree.cpp
@@ -2,28 +2,45 @@
 // Created by Marti on 19.12.2017.
 //
 #include "avlTree.h"
-void avlTree::upIn(node* start) {
-    if(start != nullptr && start->previous != nullptr){
-        node* before = start->previous;
+
+void avlTree::upIn(node *start) {
+    if (start != nullptr && start->previous != nullptr) {
+        node *before = start->previous;
         //check if node is on left or right side of the previous node.
-        if(before->left == start){
+        if (before->left == start) {
             //check what balance the previous node has/had
-            if(before->balance == 1){
+            if (before->balance == 1) {
                 before->balance -= 1; //balance is now 0 --> no more upInOut needed.
-            }
-            else if(before->balance == 0){
+            } else if (before->balance == 0) {
                 before->balance -= 1;
                 upIn(before);
+            } else if (before->balance == -1) {
+                if (start->balance == -1){
+                    rotateRight(before);
+                }
+                else{
+                    rotateLeft(start);
+                    rotateRight(before);
+                }
             }
-            else{
-                //TODO double roattion or normal rotation.
+        } else {
+            if (before->balance == 1) {
+                before->balance += 1;
+            } else if (before->balance == 0) {
+                before->balance += 1;
+                upIn(before);
+            } else if (before->balance == -1) {
+                if (start->balance == -1){
+                    rotateLeft(before);
+                }
+                else{
+                    rotateRight(start);
+                    rotateLeft(before);
+                }
             }
-            //TODO leaf was right side.
-        } else{
-
         }
-        //============================================================
     }
+    //============================================================
 }
 
 void avlTree::rotateLeft() {
@@ -37,75 +54,69 @@ void avlTree::rotateRight() {
 //previous = true:  if the search reaches a leaf it returns the previous node holding the leaf. Only if the tree is empty it returns a nullptr.
 //                  if a node is returned and not nullptr it must be checked if the returned node is the searched for key or a different one.
 //previous = false: if the search reaches a leaf it returns a nullptr. otherwise it returns the node with the searched for key.
-node* avlTree::searchRecursive(node *start, const int key,bool previous){
-    if(start == nullptr) return nullptr;
-    node* p = start;
-    if(k == p->key) {
+node *avlTree::searchRecursive(node *start, const int key, bool previous) {
+    if (start == nullptr) return nullptr;
+    node *p = start;
+    if (k == p->key) {
         return p;
-    }
-    else if(key < p->key){
-        if(p->left == nullptr && !previous) return nullptr;
-        if(p->left == nullptr && previous) return p;
+    } else if (key < p->key) {
+        if (p->left == nullptr && !previous) return nullptr;
+        if (p->left == nullptr && previous) return p;
         searchRecursive(p->left, key);
-    }
-    else {
-        if(p->right == nullptr && !previous) return nullptr;
-        if(p->right == nullptr && previous) return p;
-        searchRecursive(p->right,key);
+    } else {
+        if (p->right == nullptr && !previous) return nullptr;
+        if (p->right == nullptr && previous) return p;
+        searchRecursive(p->right, key);
     }
 }
 
 bool avlTree::search(const int key) {
     bool notPrevious = false;
-    node* searchedNode = searchRecursive(firstNode,key, notPrevious);
-    if(searchedNode == nullptr){
+    node *searchedNode = searchRecursive(firstNode, key, notPrevious);
+    if (searchedNode == nullptr) {
         return false;
-    }
-    else{
+    } else {
         return true;
     }
 }
 
 bool avlTree::insert(const int key) {
     bool previous = true;
-    node* insertNode = searchRecursive(firstNode, key, previous);
+    node *insertNode = searchRecursive(firstNode, key, previous);
     //Only way searchRecursive returns nullPtr (with previous flag = true) is when tree is empty.
-    if(insertNode == nullptr) insertNode = new node(key, nullptr);
+    if (insertNode == nullptr) insertNode = new node(key, nullptr);
     //If anything else than nullptr is returned check if key already exists or if a previous node was returned.
-    if(insertNode->key == key) return false;
+    if (insertNode->key == key) return false;
     //If key does not exist insert left or right.
-    if(key < insertNode->key){
-        insertNode->left = new node(key,insertNode);
+    if (key < insertNode->key) {
+        insertNode->left = new node(key, insertNode);
         insertNode->balance -= 1;
-        if(insertNode->balance != 0){
+        if (insertNode->balance != 0) {
             upIn(insertNode);
         }
-    }
-    else {
-        insertNode->right = new node(key,insertNode);
+    } else {
+        insertNode->right = new node(key, insertNode);
         insertNode->balance += 1;
-        if(insertNode->balance != 1){
+        if (insertNode->balance != 1) {
             upIn(insertNode);
         }
 
-}
-
-void avlTree::remove() {
-
-}
-
-void avlTree::node::calculateBalance() {
-    if((this->right && this->left) == nullptr){
-        this.balance = 0;
     }
-    else if(this->right == nullptr){
-        this->balance = -left->balance;
+
+    void avlTree::remove() {
+        //TODO remove
     }
-    else if(this->left == nullptr){
-        this->balance = right->balance;
-    }
-    else{
-        this->balance = right->balance - left->balance;
+
+    void avlTree::node::calculateBalance() {
+        if ((this->right && this->left) == nullptr) {
+            this.balance = 0;
+        } else if (this->right == nullptr) {
+            this->balance = -left->balance;
+        } else if (this->left == nullptr) {
+            this->balance = right->balance;
+        } else {
+            this->balance = right->balance - left->balance;
+        }
     }
 }
 
