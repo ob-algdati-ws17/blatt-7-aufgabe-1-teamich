c7fb6439f0350be9f14dbe0e4f3a934a5484ab52
Martin Gaiser <martin-gaiser@t-online.de>
2017-12-22 11:54:57 +0100

    added more tests

--- test/testAvlTree.cpp
+++ test/testAvlTree.cpp
@@ -11,6 +11,23 @@ TEST(testAvlTree,TestInsertOne) {
     EXPECT_TRUE(tree.search(1));
 }
 
+TEST(testAvlTree,TestInsertBiggerAndLower){
+    avlTree tree;
+    EXPECT_TRUE(tree.insert(2));
+    EXPECT_TRUE(tree.search(2));
+    EXPECT_TRUE(tree.insert(3));
+    EXPECT_TRUE(tree.search(3));
+    EXPECT_TRUE(tree.insert(1));
+    EXPECT_TRUE(tree.search(1));
+}
+
+TEST(textAvlTree, TestInsertDouble){
+    avlTree tree;
+    EXPECT_TRUE(tree.insert(1));
+    EXPECT_FALSE(tree.insert(1));
+    EXPECT_TRUE(tree.search(1));
+}
+
 
 
 // empty list
